6c886d3aedc0baf0ab58fd1a7c2a4a9984cea07d
Zukhriddin Jabborov <j.zuhriddin.95@gmail.com>
2023-06-15 11:42:06 +0900

    Class Inheritence, Synchrone/Asynchrone, Error handling and Callback

--- lesson17.js
+++ lesson17.js
@@ -34,6 +34,10 @@ user.getPackage();
 // This means that if there is a synchronous operation that takes a significant amount of time to complete,
 // it will block the execution of other code and potentially make the program unresponsive.
 
+
+// ERROR HANDLING
+// TRY - muammoga sababchi bukishi mmkn bulgan code.
+// CATCH - muammoni chiqaradi.
 try{
     let name = 'Zuhriddin';
     console.log(name);
@@ -41,4 +45,43 @@ try{
     console.log(error.name);
     console.log(error.message);
     console.log(error.stack);
+};
+
+
+
+// CALLBACK
+// THIS IS BRIEF EXPLANATION:
+// First, there is an object called users that stores a username and password.
+// Then, there is a function called facebook that takes three parameters: id, pwd, and callback.
+// This function simulates a login process by checking if the provided id and pwd match the values stored in the users object.
+// Inside the facebook function, there is a setTimeout function that delays the execution of the login process by 
+// 2000 milliseconds (2 seconds). This is done to simulate a network delay or server response time.
+// After the delay, the function checks if the provided id and pwd match the values stored in the users object. 
+// If they match, it calls the callback function with the message "Welcome to Facebook." If they don't match, 
+// it calls the callback function with the message "Password or ID is wrong."
+// Finally, there is a variable called member that calls the facebook function with the username "Sophia" and password "1999". 
+// It also provides a callback function that logs the received data to the console.
+// When the code is executed, it will output either "Welcome to Facebook" if the provided username and password are correct, 
+// or "Password or ID is wrong" if they are incorrect. The output will be logged to the console after a 2-second delay.
+
+let users = {
+    username: 'Sophia',
+    password: '1999',
 }
+
+const facebook = (id, pwd, callback) => {
+
+    setTimeout(()=> {
+        if(users.username === id && users.password === pwd){
+            callback ('Welcome to Facebook')
+        }else{
+            callback ('Password or ID is wrong')
+        }
+    }, 2000)
+
+}
+
+const member = facebook('Sophia', '1999', (data)=> {
+    console.log(data);
+});
+
